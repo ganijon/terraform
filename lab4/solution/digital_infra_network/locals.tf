b76cd20cfb1e494f06cb462144e2b06dc253fb91
ganijon <ganijon.rahimov@gmail.com>
2023-11-08 18:54:11 -0500

    remove lab4 solution

--- lab4/solution/digital_infra_network/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  resource_lowercase_array  = [lower(var.environment), substr(lower(var.location), 0, 2), lower(var.domain), var.resource_group_name_suffix]
-  resource_suffix_kebabcase = join("-", local.resource_lowercase_array)
-}
-
