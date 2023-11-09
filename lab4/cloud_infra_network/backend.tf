b558e63e171cd9559d7800d4413cf48f99bdec9b
ganijon <ganijon.rahimov@gmail.com>
2023-11-08 20:13:31 -0500

    use shell backend-config

--- lab4/cloud_infra_network/backend.tf
+++ lab4/cloud_infra_network/backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "rg-tf-state-eus-dev-02"
-    storage_account_name = "sttfstateeusdev02"
-    container_name = "tfstates"
-    key = "network/terraform.state"
+    # backend-config done via 'init.sh'
   }
 }
